76ee71cd21d3feb16dedabaa846b69ace5f15563
Lohith <lohithpoojary64@gmail.com>
2024-11-01 12:42:03 +0530

    working on about component

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,14 +1,15 @@
+import About from '@/components/About/About'
 import BasedIn from '@/components/About/BasedIn'
 import Header from '@/components/About/Header'
 import React from 'react'
 
 const page = () => {
   return (
-    <div className='min-h-screen h-screen w-full px-8 bg-slate-100 '>
+    <div className='min-h-screen h-screen w-full px-8 bg-slate-100  overflow-y-scroll '>
       <Header />
       <div className="border border-dashed border-gray-400"></div>
       <BasedIn/>
-
+      <About/>
     </div>
   )
 }
---
+++ src/components/About/About.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <div className='h-[300px] w-full flex justify-between items-center mt-20'>
+            <div className='h-full w-[40%]'>
+                <h1 className='font-bold text-xl'>Hello, I'm Lohith Poojary</h1>
+                <h1 className='text-md text-slate-500 mt-4'>A passionate Junior Product Designer with a strong background in motion design and illustration. With a deep love for creativity and innovation, I’ve crafted engaging visual experiences and intuitive, user-friendly interfaces.<br/>
+
+                    Previously, I created animated content on Instagram, which allowed me to refine my skills in storytelling through motion. My work focuses on delivering designs that exceed user expectations while staying updated with the latest trends to ensure fresh and relevant results.</h1>
+            </div>
+        </div>
+    )
+}
+
+export default About
