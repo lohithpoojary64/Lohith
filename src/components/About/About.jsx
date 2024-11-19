a3dd8336113e6bdb282c4c4f59041894a14e8d5b
Lohith <lohithpoojary64@gmail.com>
2024-11-19 08:37:08 +0530

    changes some text

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -7,9 +7,9 @@ const About = () => {
         <div className='h-[300px] lg:h-[350px] w-full flex justify-between items-center mt-20'>
             <div className='h-full w-full lg:w-[40%]'>
                 <h1 className='font-bold text-xl dark:text-white'>Hello, I'm Lohith Poojary</h1>
-                <h1 className='text-md text-slate-500 mt-4 dark:text-slate-300'>A passionate Junior Product Designer with a strong background in motion design and illustration. With a deep love for creativity and innovation, I’ve crafted engaging visual experiences and intuitive, user-friendly interfaces.<br />
+                <h1 className='text-md text-slate-500 mt-4 dark:text-slate-300'>A passionate Front End Developer. I am constantly exploring new technologies and frameworks to stay ahead in the ever-evolving landscape of web development. My goal is to create seamless and visually appealing applications that not only meet client needs but also provide an enjoyable user experience.<br />
 
-                    Previously, I created animated content on Instagram, which allowed me to refine my skills in storytelling through motion. My work focuses on delivering designs that exceed user expectations while staying updated with the latest trends to ensure fresh and relevant results.</h1>
+                    I am always open to networking and collaboration opportunities. Feel free to reach out if you want to discuss projects, share ideas, or explore potential partnerships!</h1>
             </div>
             <div className='hidden lg:block h-full lg:w-[40%] rounded-xl overflow-hidden'>
                 <Image src={lohith} alt='My-Image' height={5000} width={5000} className='h-full w-full bg-cover' ></Image>
